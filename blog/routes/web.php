607d6fe778dc8e7802762f4ff4051da89d0f4bf8
Adnane Kes <tangerinoh40@gmail.com>
2025-10-14 12:18:56 +0100

    done

---
+++ blog/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+    
+});
+
+
+// routes/web.php
+
+Route::get('/ping', fn() => 'pong');
