88cb090d4b2090c82f1cab24f01ccf5ed2c7a13d
Adnane Kes <tangerinoh40@gmail.com>
2025-11-14 09:40:58 +0000

    done

--- blog/app/Http/Requests/UpdateArticleRequest.php
+++ blog/app/Http/Requests/UpdateArticleRequest.php
@@ -3,26 +3,41 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Str;
 
 class UpdateArticleRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'slug' => $this->input('slug') ?: Str::slug($this->input('title'))
+        ]);
+    }
+
     public function rules(): array
     {
+        $article = $this->route('article');
+
         return [
-            //
+            'title'   => ['required','string','min:3','max:150'],
+            'slug'    => [
+                'required','string','max:180',
+                Rule::unique('articles','slug')->ignore($article)
+            ],
+            'content' => ['required','string','min:20'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'slug.unique' => 'Ce slug est déjà pris par un autre article.',
         ];
     }
 }
